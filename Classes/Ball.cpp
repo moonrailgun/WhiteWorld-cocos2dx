3121ac886638045e463fd7f62c1023b7aae418bd
moonrailgun <moonrailgun@gmail.com>
2016-03-16 16:07:53 +0800

    ..
    ..

---
+++ Classes/Ball.cpp
@@ -0,0 +1,9 @@
+#include "Ball.h"
+
+USING_NS_CC;
+
+bool Ball::init() {
+	initWithFile("CloseNormal.png");
+
+	return true;
+}
---
+++ Classes/Ball.h
@@ -0,0 +1,9 @@
+#include "cocos2d.h"
+
+class Ball : public cocos2d::Sprite
+{
+public:
+	virtual bool init();
+
+	CREATE_FUNC(Ball);
+};
