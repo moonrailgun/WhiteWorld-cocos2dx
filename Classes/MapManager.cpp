296d5349b7a4b2486804d0983530d0cec031eaae
moonrailgun <moonrailgun@gmail.com>
2016-03-31 17:25:51 +0800

    鼠标指向地块高亮显示
    ADD 鼠标指向地块高亮显示

--- Classes/MapManager.cpp
+++ Classes/MapManager.cpp
@@ -2,6 +2,7 @@
 #include "cocos2d.h"
 #include "SimpleAudioEngine.h"
 #include "GameLayer.h"
+#include "GlobalData.h"
 
 USING_NS_CC;
 bool MapManager::init(){
@@ -15,6 +16,8 @@ bool MapManager::init(){
 	selectedBlock = NULL;
 	selectedBlockHighLight = NULL;
 
+	this->retain();//保留对象所有权，使对象不会被释放
+
 	return true;
 }
 
@@ -25,7 +28,8 @@ MapManager* MapManager::createManager(GameLayer* gameLayer){
 	return mapManager;
 }
 
-bool MapManager::loadMap(char *mapName,int mapZoom){
+//加载地图
+bool MapManager::loadMap(char *mapName, int mapZoom){
 	auto name = String::createWithFormat("map/%s.tmx", mapName);
 	log("is loading map which named %s [%s]", mapName, name->getCString());
 	_currentMapName = mapName;
@@ -36,9 +40,9 @@ bool MapManager::loadMap(char *mapName,int mapZoom){
 		return false;
 	}
 
-	_currentMapSize =Size(_currentMap->getMapSize());
-	_tileSize =Size(_currentMap->getTileSize());
-	
+	_currentMapSize = Size(_currentMap->getMapSize());
+	_tileSize = Size(_currentMap->getTileSize());
+
 	_currentMap->setScale(_mapZoom);
 
 	if (_gameLayer == NULL){
@@ -65,15 +69,33 @@ void MapManager::setSelectedHighlight(){
 	Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, _gameLayer);
 }
 
-void MapManager::onMouseMove(EventMouse *mouse){
+void MapManager::onMouseMove(EventMouse *mouse) {
 	Vec2 mousePos = Vec2(mouse->getCursorX(), mouse->getCursorY());
-	//TMXLayer* bottomLayer = _currentMap->getLayer("bottom");
-	
-	this->selectedBlockHighLight->clear();
-	this->selectedBlockHighLight->drawRect(mousePos, mousePos + Vec2(32, 32), Color4F(1, 0, 0, 1));
+	TMXLayer* bottomLayer = _currentMap->getLayer("bottom");
 
+	auto mapTileSize = bottomLayer->getMapTileSize() * getMapZoom();//地块实际大小
+	auto map = getMap();
+	auto mapSize = Size(map->getMapSize().width * mapTileSize.width, map->getMapSize().height * mapTileSize.height);//地图实际大小
+	auto mapAnchor = map->getAnchorPoint();
+
+	Vec2 mapStartPoint = Vec2(map->getPositionX() - mapSize.width * mapAnchor.x, map->getPositionY() - mapSize.height * mapAnchor.y);//地图左下角坐标
+
+	mousePos.clamp(mapStartPoint + Vec2(0.1, 0.1), mapStartPoint + mapSize - Vec2(0.1, 0.1));//限制在地图内,距离地图边缘0.1像素
+
+	Vec2 tileCoordinate = Vec2(mousePos.x / mapTileSize.width, mousePos.y / mapTileSize.height);
+	log("%f,%f", tileCoordinate.x, tileCoordinate.y);
+	Sprite* tileSelected = bottomLayer->getTileAt(Vec2((int)tileCoordinate.x, map->getMapSize().height - 1 - (int)tileCoordinate.y));//获取选中地块的精灵
+	auto tileSelectedPos = tileSelected->getPosition();//地块相对坐标
+	auto tileSelectedRealPos = tileSelectedPos * getMapZoom() + Vec2(mapSize.width * mapAnchor.x, mapSize.height*mapAnchor.y) + mapStartPoint; //地块真实像素坐标
+
+	selectedBlockHighLight->clear();
+	//selectedBlockHighLight->drawRect(mousePos, mousePos + Vec2(32, 32), Color4F(1, 0, 0, 1));
+	selectedBlockHighLight->drawRect(tileSelectedRealPos, tileSelectedRealPos + mapTileSize, Color4F(1, 0, 0, 1));//地块高亮绘制
 }
 
+GameLayer* MapManager::getGameLayer(){
+	return this->_gameLayer;
+}
 TMXTiledMap* MapManager::getMap(){
 	return this->_currentMap;
 }
