1c91f0a64b0b99406b9a6fe3b4cf0b8bde47415d
moonrailgun <moonrailgun@gmail.com>
2016-04-02 20:16:40 +0800

    动画工具
    ADD 动画工具

---
+++ Classes/ActionTool.cpp
@@ -0,0 +1,25 @@
+﻿#include "ActionTool.h"
+
+Animate* ActionTool::animationWithFrameName(const char* frameName, int iloops, float delay){
+	SpriteFrame* frame = NULL;
+	Animation* animation = Animation::create();
+	int index = 1;
+	// 遍历具有frameName特征的图片帧
+	do
+	{
+		String * name = String::createWithFormat("%s%02d.png", frameName, index++);   // 当前是第index帧
+		frame = SpriteFrameCache::getInstance()->getSpriteFrameByName(name->getCString());
+		if (frame == NULL)
+		{
+			break;
+		}
+		animation->addSpriteFrame(frame);
+	} while (true);
+	// 设置Animation的一些基本属性
+	animation->setDelayPerUnit(delay);
+	animation->setRestoreOriginalFrame(true);
+	animation->setLoops(iloops);
+	Animate* animate = Animate::create(animation);
+	// 返回一个Animate
+	return animate;
+}
---
+++ Classes/ActionTool.h
@@ -0,0 +1,13 @@
+﻿#ifndef __ActionTool_H__
+#define __ActionTool_H__
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+class ActionTool {
+public:
+	static Animate* animationWithFrameName(const char *frameName, int iloops, float delay);
+	static Animate* animationWithFrameAndNum(const char *frameName, int num, float delay);
+};
+
+#endif
