c270e596c94ff560c84461b3594bf15fa38705a0
moonrailgun <moonrailgun@gmail.com>
2016-04-09 16:07:31 +0800

    获取Node点位置辅助类
    ADD 获取Node点位置辅助类

---
+++ Classes/PointHelper.cpp
@@ -0,0 +1,40 @@
+﻿#include "PointHelper.h"
+
+Vec2 PointHelper::getNodeLeftTopPoint(Node* node){
+	Vec2 point = node->getPosition() - Vec2(node->getAnchorPoint().x * node->getContentSize().width, -node->getAnchorPoint().y * node->getContentSize().height);
+	return point;
+}
+Vec2 PointHelper::getNodeLeftBottomPoint(Node* node){
+	Vec2 point = node->getPosition() - Vec2(node->getAnchorPoint().x * node->getContentSize().width, node->getAnchorPoint().y * node->getContentSize().height);
+	return point;
+
+}
+Vec2 PointHelper::getNodeRightTopPoint(Node* node){
+	Vec2 point = node->getPosition() + Vec2(node->getAnchorPoint().x * node->getContentSize().width, node->getAnchorPoint().y * node->getContentSize().height);
+	return point;
+
+}
+Vec2 PointHelper::getNodeRightBottomPoint(Node* node){
+	Vec2 point = node->getPosition() + Vec2(node->getAnchorPoint().x * node->getContentSize().width, -node->getAnchorPoint().y * node->getContentSize().height);
+	return point;
+}
+Vec2 PointHelper::getNodeCenterPoint(Node* node){
+	Vec2 point = PointHelper::getNodeLeftBottomPoint(node) + node->getContentSize() / 2;
+	return point;
+}
+Vec2 PointHelper::getNodeMiddleTopPoint(Node* node){
+	Vec2 point = PointHelper::getNodeCenterPoint(node) + Vec2(0, node->getContentSize().height / 2);
+	return point;
+}
+Vec2 PointHelper::getNodeMiddleBottomPoint(Node* node){
+	Vec2 point = PointHelper::getNodeCenterPoint(node) - Vec2(0, node->getContentSize().height / 2);
+	return point;
+}
+Vec2 PointHelper::getNodeMiddleRightPoint(Node* node){
+	Vec2 point = PointHelper::getNodeCenterPoint(node) + Vec2(node->getContentSize().width / 2, 0);
+	return point;
+}
+Vec2 PointHelper::getNodeMiddleLeftPoint(Node* node){
+	Vec2 point = PointHelper::getNodeCenterPoint(node) - Vec2(node->getContentSize().width / 2, 0);
+	return point;
+}
---
+++ Classes/PointHelper.h
@@ -0,0 +1,21 @@
+﻿#ifndef __PointHelper__H__
+#define __PointHelper__H__
+
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class PointHelper{
+public:
+	static Vec2 getNodeLeftTopPoint(Node* node);
+	static Vec2 getNodeLeftBottomPoint(Node* node);
+	static Vec2 getNodeRightTopPoint(Node* node);
+	static Vec2 getNodeRightBottomPoint(Node* node);
+	static Vec2 getNodeCenterPoint(Node* node);
+	static Vec2 getNodeMiddleTopPoint(Node* node);
+	static Vec2 getNodeMiddleBottomPoint(Node* node);
+	static Vec2 getNodeMiddleRightPoint(Node* node);
+	static Vec2 getNodeMiddleLeftPoint(Node* node);
+};
+
+#endif
