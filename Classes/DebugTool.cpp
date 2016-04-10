fcb822f39f0fb30a929a52054ea1b8918ee64815
moonrailgun <moonrailgun@gmail.com>
2016-04-10 13:34:11 +0800

    DebugTool
    ADD DebugTool

---
+++ Classes/DebugTool.cpp
@@ -0,0 +1,16 @@
+﻿#include "DebugTool.h"
+
+void DebugTool::log(Vec2 vec2){
+	cocos2d::log("[Debug]Vec2: (%f,%f)", vec2.x, vec2.y);
+}
+void DebugTool::log(Size size){
+	cocos2d::log("[Debug]Size: (%f,%f)", size.width, size.height);
+}
+void DebugTool::log(Node* node){
+	cocos2d::log("[Debug]Node: pos->(%f,%f), size->(%f,%f), tag->%d", 
+		node->getPositionX(),
+		node->getPositionY(),
+		node->getContentSize().width, 
+		node->getContentSize().height, 
+		node->getTag());
+}
---
+++ Classes/DebugTool.h
@@ -0,0 +1,14 @@
+﻿#ifndef __DebugTool__H__
+#define __DebugTool__H__
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+class DebugTool{
+public:
+	static void log(Vec2 vec2);
+	static void log(Size size);
+	static void log(Node node);
+};
+
+#endif
